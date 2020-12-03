163ee8cf9b5ef588ff196a5c12655908f61226d5
TaricEastDog <malzusiek666@gmail.com>
2020-12-03 21:48:39 +0100

    signin auth and error handling

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { FirebaseContext } from "../context/firebase";
 import { Header, Form } from "../components";
 
 export default function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const { firebase_ } = useContext(FirebaseContext);
+
+  const signUpHandler = (e) => {
+    e.preventDefault();
+
+    firebase_
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        setEmail("");
+        setPassword("");
+        setError(err.message);
+      });
+  };
+
   return (
     <Header>
       <Header.Frame>
@@ -10,9 +33,20 @@ export default function Signin() {
 
       <Form>
         <Form.Title>Sign In</Form.Title>
-        <Form.Base>
-          <Form.Input placeholder="Email" />
-          <Form.Input placeholder="Password" />
+        {error ? <Form.Error>{error}</Form.Error> : null}
+        <Form.Base onSubmit={signUpHandler}>
+          <Form.Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            placeholder="Email"
+          />
+          <Form.Input
+            type="password"
+            autocomplete="off"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            placeholder="Password"
+          />
           <Form.SubmitButton>Sign In</Form.SubmitButton>
         </Form.Base>
         <Form.Text>
